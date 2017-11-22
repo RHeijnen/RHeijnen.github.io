0c473e082ee03633beac91750231dc3ebe095170
Orlando1991 <osd_1991@hotmail.com>
2017-11-22 13:27:33 +0100

    tapping game

---
+++ proj/g5/game5.js
@@ -0,0 +1,40 @@
+window.addEventListener('load', function(){ // on page load
+    var canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight   
+    var initialDistance = 5
+    var distance = initialDistance //distance between you and person chasing
+    var duration = 20 //countdown timer in seconds
+    startCountdowns(duration)
+
+    canvas.addEventListener("touchstart", function (e) {  
+        //increase distance by tapping
+        distance++
+        console.log("tap")
+    });
+
+    function startCountdowns(duration){
+        var timer = duration
+        setInterval(function(){
+            //timer will go down every second
+            timer--
+            console.log(timer + " seconds left")
+            
+            //distance will decrease every second
+            distance--
+            console.log(distance)
+
+            //timeer reaches 0, you win
+            if(timer == 0){
+                alert("Winner")
+            }
+
+            //distance = 0, you got caught, you lose
+            if(distance == 0){
+                timer = duration
+                distance = initialDistance
+                alert("Loser, try again")
+            }
+        }, 1000)
+    }
+}, false)
