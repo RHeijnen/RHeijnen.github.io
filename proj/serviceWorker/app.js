c97fb59d5cdf02788267e61c942209aa7c982449
Rene Heijnen <heijnenrene@gmail.com>
2017-11-29 14:07:35 +0100

    d
    changes

--- proj/serviceWorker/app.js
+++ proj/serviceWorker/app.js
@@ -1,11 +1,23 @@
-if('serviceWorker' in navigator){
+// if('serviceWorker' in navigator){
 
-    navigator.serviceWorker
-        .register('./service-worker.js', { scope: './' })
-        .then(function(registration){
-            console.log("Service Worker Registered", registration.scope);
-        })
-        .catch(function(err){
-            console.log("Service worker failed to register", err);
-        })
-}
+//     navigator.serviceWorker
+//         .register('./service-worker.js', { scope: './' })
+//         .then(function(registration){
+//             console.log("Service Worker Registered", registration.scope);
+//         })
+//         .catch(function(err){
+//             console.log("Service worker failed to register", err);
+//         })
+// }
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
