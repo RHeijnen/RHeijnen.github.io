d9bec02766d930d14d12548a2b4e9c6e28c30441
Rene Heijnen <heijnenrene@gmail.com>
2017-11-29 14:09:50 +0100

    happy orlando
    adjusted URL

--- proj/serviceWorker/app.js
+++ proj/serviceWorker/app.js
@@ -12,7 +12,7 @@
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      navigator.serviceWorker.register('/serviceWorker/service-worker.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function(err) {
