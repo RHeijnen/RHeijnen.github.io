527fa8d0aee1fb17953d55dc7cd670d936955c8c
Rene Heijnen <heijnenrene@gmail.com>
2017-11-30 10:38:41 +0100

    try 2
    another installer attempt

--- proj/serviceWorker/app.js
+++ proj/serviceWorker/app.js
@@ -1,17 +1,24 @@
-// if('serviceWorker' in navigator){
-
-//     navigator.serviceWorker
-//         .register('./service-worker.js', { scope: './' })
-//         .then(function(registration){
-//             console.log("Service Worker Registered", registration.scope);
-//         })
-//         .catch(function(err){
-//             console.log("Service worker failed to register", err);
-//         })
-// }
-
   navigator.serviceWorker.register('./worker.js').then(function(reg) {
     console.log('◕‿◕', reg);
   }, function(err) {
     console.log('ಠ_ಠ', err);
+  });
+
+
+  window.addEventListener('beforeinstallprompt', function(e) {
+    // beforeinstallprompt Event fired
+  
+    // e.userChoice will return a Promise.
+    // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
+    e.userChoice.then(function(choiceResult) {
+  
+      console.log(choiceResult.outcome);
+  
+      if(choiceResult.outcome == 'dismissed') {
+        console.log('User cancelled home screen install');
+      }
+      else {
+        console.log('User added to home screen');
+      }
+    });
   });
