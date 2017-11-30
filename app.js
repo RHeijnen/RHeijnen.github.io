bc64932e626ea1a74579c8a0b6dfeb0e11e9dc21
Rene Heijnen <heijnenrene@gmail.com>
2017-11-30 11:03:51 +0100

    refac
    refact pt2

--- app.js
+++ app.js
@@ -3,17 +3,9 @@
   }, function(err) {
     console.log('ಠ_ಠ', err);
   });
-
-
   window.addEventListener('beforeinstallprompt', function(e) {
-    // beforeinstallprompt Event fired
-  
-    // e.userChoice will return a Promise.
-    // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
     e.userChoice.then(function(choiceResult) {
-  
       console.log(choiceResult.outcome);
-  
       if(choiceResult.outcome == 'dismissed') {
         console.log('User cancelled home screen install');
       }
